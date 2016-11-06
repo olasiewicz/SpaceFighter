8a95e82d1e225e933ea18cf9d6f458543c57c303
Olasiewicz <wojciech.olasiewicz@mobica.com>
2016-11-06 16:47:08 +0100

    added star generator

---
+++ Assets/MyScripts/star.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class star : MonoBehaviour {
+
+
+	public float speed; //speed of the star
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//get current possision of the star
+		Vector2 position = transform.position;
+
+		//compute star's new position
+		position = new Vector2(position.x, position.y + speed * Time.deltaTime);
+
+		//update the star's position
+		transform.position = position;
+
+		//bottom - left point of the screen
+		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2(0, 0));
+
+		//top - right point of the screen
+		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2(1, 1));
+
+		//when star goes outside bottom of the screen, then set randomly position on top of the screen
+		if (transform.position.y < min.y) {
+			transform.position = new Vector2(Random.Range(min.x, max.x), max.y);
+		}
+	
+	}
+}
---
+++ Assets/MyScripts/starGenerator.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class starGenerator : MonoBehaviour {
+
+	public GameObject starGameObject;// starGameObject prefab
+	public int maxStars; //number of stars
+
+	//Aray of colors
+	Color[] starColors = {
+		new Color(0.5f, 0.5f, 1f), //blue
+		new Color(0, 1f, 1f), //green
+		new Color(1f, 1f, 0), //yellow
+		new Color(1f, 0, 0) //red
+	};
+
+	// Use this for initialization
+	void Start () {
+
+		//bottom - left point of the screen
+		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2(0, 0));
+
+		//top - right point of the screen
+		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2(1, 1));
+
+		//loop to create the stars
+
+		for(int i = 0; i < maxStars; i++) {
+			GameObject star = (GameObject)Instantiate (starGameObject);
+
+			//set the star color
+			star.GetComponent<SpriteRenderer>().color = starColors[i % starColors.Length];
+
+			//set random position of the star
+			star.transform.position = new Vector2(Random.Range(min.x, max.x), Random.Range(min.y, max.y));
+
+			//set random speed for the star
+			star.GetComponent<star>().speed = -(1f * Random.value + 0.5f);
+
+			//make the star a child of the starGeneratorGameObject
+			star.transform.parent = transform;
+
+
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
